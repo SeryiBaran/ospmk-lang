ba834ee9f8aee12ffce6ae4ec84d824372e68bf0
SeryiBaran <seryibaranminepy@yandex.ru>
2022-06-02 21:50:23 +0300

    Init commit

---
+++ index.js
@@ -0,0 +1,47 @@
+/*
+ * ОСПМКЯП
+ * Очень Странный Простой Малофункциональный Кроссплатформенный Язык Программирования
+ * Разработчик: @SeryiBaran (github.com)
+ */
+
+const fs = require("fs");
+const path = require("path");
+const codeFile = path.join(__dirname, "code.ospmk");
+
+let counter;
+
+const operations = {
+  "!": () => {
+    console.log('"!": Hello, World!');
+  },
+  "@": () => {
+    counter = 0;
+    console.log('"@": init/re-init counter (default 0)');
+  },
+  ">": () => {
+    counter++;
+    console.log('">": increment counter');
+  },
+  "<": () => {
+    counter--;
+    console.log('"<": decrement counter');
+  },
+  "?": () => {
+    console.log(`\"?\": counter is ${counter}`);
+  },
+};
+
+fs.readFile(codeFile, { encoding: "utf-8" }, (err, data) => {
+  if (!err) {
+    data.replace(/\s/g, "").split(":").forEach((text) => {
+      let operation = operations[text];
+      if (text) {
+        if (operation) {
+          operation();
+        }
+      }
+    });
+  } else {
+    console.log(err);
+  }
+});
